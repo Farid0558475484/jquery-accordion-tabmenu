a6577155a160e6ddb85d9aa22c5737426ee236d9
Farid0558475484 <faridmh@code.edu.az>
2022-11-02 18:37:11 +0400

    qjuery

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+//accordion
+$(function() {
+	
+
+	$(".accordion__title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("accordion-active")) {
+			$(".accordion__content").slideUp(400);
+			$(".accordion__title").removeClass("accordion-active");
+			$('.accordion__arrow').removeClass('accordion__rotate');
+		}
+
+		$this.toggleClass("accordion-active");
+		$this.next().slideToggle();
+		$('.accordion__arrow',this).toggleClass('accordion__rotate');
+	});
+
+	
+});
+
+
+// tabs menu
+$(function() {
+	var tab = $('#tabs .tabs-items > div'); 
+	tab.hide().filter(':first').show(); 
+	
+
+	$('#tabs .tabs-nav a').click(function(){
+		tab.hide(); 
+		tab.filter(this.hash).show(); 
+		$('#tabs .tabs-nav a').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	}).filter(':first').click();
+ 
+
+	$('.tabs-target').click(function(){
+		$('#tabs .tabs-nav a[href=' + $(this).attr('href')+ ']').click();
+	});
+	
+
+	if(window.location.hash){
+		$('#tabs-nav a[href=' + window.location.hash + ']').click();
+		window.scrollTo(0, $("#" . window.location.hash).offset().top);
+	}
+});
